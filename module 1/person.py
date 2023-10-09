6701b53abd7bb3ab00990913eefe9c6f48a20ffa
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2023-10-09 15:36:22 +0200

    zieke update

---
+++ module 1/person.py
@@ -0,0 +1,11 @@
+naam = str(input("wat is u voor en achter naam?: "))
+adres = str(input("wat is u adres?: "))
+postcode = str(input("wat is u postcode?: "))
+woonplaats = str(input("wat is u woonplaats?: "))
+
+print(" ----------------------------------------------------")
+print(f"|  Naam      : {naam}                ")
+print(f"|  Adres     : {adres}           ")
+print(f"|  Postcode  : {postcode}                         ")
+print(f"|  Woonplaats: {woonplaats}                          ")
+print(" ----------------------------------------------------")
