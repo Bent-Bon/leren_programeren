f904660b1693d8403f2d88d81a95057fce3b7444
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2023-10-09 15:20:38 +0200

    update

--- module 1/speelhal.py
+++ module 1/speelhal.py
@@ -1,23 +1,24 @@
 from termcolor import colored
-prijs_dag = 7.45
-prijs_vip = 0.074
+PRIJS_DAG = 7.45
+PRIJS_VIP_5_MINUTEN =0.37
+prijs_vip_minuut = PRIJS_VIP_5_MINUTEN / 5
 
 
 
-print(colored("dagkaart €7.45", "yellow"))
+print(colored(f"dagkaart €{PRIJS_DAG}", "yellow"))
 
 aantal_dagkaart = int(input("hoeveel tickets wil je?: "))
 
-kosten_dagkaart = (aantal_dagkaart * prijs_dag)
+kosten_dagkaart = (aantal_dagkaart * PRIJS_DAG)
 kosten_dagkaart = round(kosten_dagkaart, 2)
 
 print(colored(f"dat is €{kosten_dagkaart}", "green"))
 
 vraag_vip = str(input("wil je de VIP-VR-GameSeat gebruiken?: ")).lower()
 if vraag_vip == "yes":
-    print(colored("5 minuten vip kost €0.37", "yellow"))
+    print(colored(f"5 minuten vip kost €{PRIJS_VIP_5_MINUTEN}", "yellow"))
     aantal_vip = int(input("hoeveel minuten wil je?: "))
-    kosten_dagkaart = kosten_dagkaart + (aantal_vip * prijs_vip)
+    kosten_dagkaart = kosten_dagkaart + (aantal_vip * prijs_vip_minuut)
     kosten_dagkaart = round(kosten_dagkaart, 2)
     print(colored(f"dan wordt dan €{kosten_dagkaart}", "green"))
     
