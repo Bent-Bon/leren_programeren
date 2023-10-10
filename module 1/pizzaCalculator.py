e994ecfb2e33840658c4606c3c9deb2d5daea906
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2023-10-10 15:40:13 +0200

    update

---
+++ module 1/pizzaCalculator.py
@@ -0,0 +1,30 @@
+#Bent Bon
+L_PIZZA = 13.99
+M_PIZZA = 9.99
+S_PIZZA = 7.99
+
+
+print("hallo wij hebben een asortiment van")
+print(f"(45 cm) grote pizza's €{L_PIZZA}")
+print(f"(30 cm) middel pizza's €{M_PIZZA}")
+print(f"(20 cm) kleine pizza's €{S_PIZZA}")
+
+groote_pizza = int(input("hoeveel grote pizza's wil je?: "))
+prijs_L_pizza = groote_pizza * L_PIZZA
+
+middel_pizza = int(input("hoeveel middel pizza's wil je?: "))
+prijs_M_pizza = (middel_pizza * M_PIZZA)
+
+kleine_pizza = int(input("hoeveel kleine pizza's wil je?: "))
+prijs_S_pizza = kleine_pizza * S_PIZZA
+
+totaal = groote_pizza + middel_pizza + kleine_pizza
+btw = totaal / 100 * 9
+btw_totaal = totaal + btw
+
+print(f"aantal groote pizza('s) {groote_pizza} €{prijs_L_pizza}")
+print(f"aantal middel pizza('s) {middel_pizza} €{prijs_M_pizza}")
+print(f"aantal kleine pizza('s) {kleine_pizza} €{prijs_S_pizza}")
+print(f"totaal €{totaal}")
+print(f"btw €{btw}")
+print(f"totaal met btw €{btw_totaal}")
