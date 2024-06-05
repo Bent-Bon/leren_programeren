9f373fdabf88632f4a8c898e534f3e87d7769771
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2024-06-05 12:04:48 +0200

    push

--- module 3/deel 1/rekenmachine/rekenmachine.py
+++ module 3/deel 1/rekenmachine/rekenmachine.py
@@ -3,7 +3,6 @@ from functions import *
 def calculator():
     func_checker_eerste_helft = ["a", "b", "c", "d", ""]
     func_checker_tweede_helft = ["e", "f", "g", "h"]
-    number1 = input("wat is je eerste nummer?: ")
     tekst = "wat wilt u doen?"
     full_som = ""
     while True:
@@ -11,11 +10,14 @@ def calculator():
             func = input(f"{tekst} \n getallen optellen, A) \n getallen aftrekken, B) \n getallen vermenigvuldigen, C) \n getallen delen, D) \n getal ophogen, E) \n getal verlagen, F) \n getal verdubbelen, G) \n getal halveren?, H) \n of niets om te stoppen").lower()
             if func not in func_checker_eerste_helft or func not in func_checker_tweede_helft:
                 print("ongeldige invoer")
+
             elif func in func_checker_eerste_helft:
                 if func == "":
                     exit()
+                number1 = input("welk getal: ")
                 number2 = input("wat is je tweede nummer?: ")
                 break
+
             elif func in func_checker_tweede_helft:
                 if func == "e" or func == "f":
                     number2 = 1
@@ -23,33 +25,48 @@ def calculator():
                     number2 = 2
                 break
 
-
         if func == "a":
             andwoord = addition(number1, number2)
             opperator = "+"
+            opperator_str = "optellen"
+
         elif func == "b":
             andwoord = subtraction(number1, number2)
             opperator = "-"
+            opperator_str = "aftrekken"
+
         elif func == "c":
             andwoord = multiplication(number1, number2)
             opperator = "*"
+            opperator_str = "vermenigvuldigen"
+
         elif func == "d":
             andwoord = division(number1, number2)
             opperator = "/"
+            opperator_str = "delen"
+
         elif func == "e":
             andwoord = addition(number1, number2)
             opperator = "+"
+            opperator_str = "ophogen"
+
         elif func == "f":
             andwoord = subtraction(number1, number2)
             opperator = "-"
+            opperator_str = "verlagen"
+
         elif func == "g":
             andwoord = subtraction(number1, number2)
             opperator = "*"
+            opperator_str = "verdubbelen"
+
         elif func == "h":
             andwoord = subtraction(number1, number2)
             opperator = "/"
+            opperator_str = "halveren"
+            
         full_som += f"{number1} {opperator} {number2} = {andwoord} \n"
-        tekst = f"{full_som} Wil je wat met de uitkomst?"
+        tekst = f"{opperator_str} \n {full_som} Wil je wat met de uitkomst?"
         number1 = andwoord
 
 calculator()
